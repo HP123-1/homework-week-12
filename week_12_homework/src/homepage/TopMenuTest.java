ffd573a4b6c3b3ec2857c7473d4a8889c0a72008
HP123-1 <hinalpandya79@gmail.com>
2021-11-24 21:49:50 +0000

    Homeworke-week-12

---
+++ week_12_homework/src/homepage/TopMenuTest.java
@@ -0,0 +1,30 @@
+package homepage;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TopMenuTest extends Utility1{
+    String baseUrl = "https://demo.nopcommerce.com/";
+
+    public void selectMenu(String menu) {
+        menu = "Computers";
+        openBrowser(baseUrl);
+        clickOnElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[1]/a[1]"));
+    }
+
+    @Test
+    public void veryFyPageNavigation() {
+        selectMenu("Computers");
+        String expectedTab = "Computers";
+        WebElement tab = driver.findElement(By.xpath("//h1[contains(text(),'Computers')]"));
+        String actualTab = tab.getText();
+        Assert.assertEquals("user is not on page", expectedTab, actualTab);
+
+    }
+}
+
+
+
